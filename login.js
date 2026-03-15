eb51590c8156a803faa385afe54466a7ead56c5c
Plink08 <lennvanderlake@gmail.com>
2026-03-15 21:01:01 +0000

    login dingetjes

--- login.js
+++ login.js
@@ -1,33 +1,27 @@
 const inputs = document.querySelectorAll(".code-inputs input");
-const secretCode = "030625";
+const secretCodeHash = "MDMwNjI1"; 
 
 inputs.forEach((input, index) => {
-
-input.addEventListener("input", () => {
-
-if(input.value.length === 1 && index < inputs.length - 1){
-inputs[index + 1].focus();
-}
-
-});
-
-input.addEventListener("keydown", (e) => {
-
-if(e.key === "Backspace" && input.value === "" && index > 0){
-inputs[index - 1].focus();
-}
-
-});
-
+  input.addEventListener("input", () => {
+    if(input.value.length === 1 && index < inputs.length - 1){
+      inputs[index + 1].focus();
+    }
+  });
+  input.addEventListener("keydown", (e) => {
+    if(e.key === "Backspace" && input.value === "" && index > 0){
+      inputs[index - 1].focus();
+    }
+  });
 });
 
 function checkCode(){
   let code = "";
   inputs.forEach(input => { code += input.value; });
 
-  if(code === secretCode){
-    // Zet flag in localStorage
-    localStorage.setItem("isLoggedIn", "true");
+  const codeHash = btoa(code); 
+
+  if(codeHash === secretCodeHash){
+    localStorage.setItem("isLoggedIn","true"); // login flag
     window.location.href = "home.html";
   } else {
     document.getElementById("error").textContent = "Wrong code 💔";
